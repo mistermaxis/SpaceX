4269b3538412cc0ef1ea65b5818e88991aaa47ab
Maximiliano <maximiliano_diaz@gmail.com>
2021-11-22 18:42:22 -0300

    Add React Router code.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Rockets from './components/rockets';
+import Missions from './components/missions';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" >
+            <Rockets />
+          </Route>
+          <Route path="/missions" >
+            <Missions />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
