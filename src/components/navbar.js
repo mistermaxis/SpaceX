7226bb5a2da71f4e48f8e23b9da7b6c3d32ff20d
Maximiliano <maximiliano_diaz@gmail.com>
2021-11-22 18:58:26 -0300

    Add navbar with links.

---
+++ src/components/navbar.js
@@ -0,0 +1,20 @@
+import { NavLink } from 'react-router-dom';
+import Planet from './planet.png';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div>
+        <img className="logo" src={Planet} alt="" />
+        <h1>Space Traveler's Hub</h1>
+      </div>
+      <menu className="menu">
+        <NavLink activeClassName="active" to="/">Rockets</NavLink>
+        <NavLink activeClassName="active" to="missions">Missions</NavLink>
+        <NavLink activeClassName="active" to="missions">My Profile</NavLink>
+      </menu>
+    </nav>
+  );
+}
+ 
+export default Navbar;
