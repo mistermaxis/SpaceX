8bfd04347458ad9b930be727276c03ce4c49a06d
Maximiliano <maximiliano_diaz@gmail.com>
2021-11-22 18:57:31 -0300

    Change Switch to Routes component.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Rockets from './components/rockets';
-import Missions from './components/missions';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/navbar.js';
+import Rockets from './components/rockets.js';
+import Missions from './components/missions.js';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Switch>
-          <Route exact path="/" >
-            <Rockets />
-          </Route>
-          <Route path="/missions" >
-            <Missions />
-          </Route>
-        </Switch>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Rockets />} />
+          <Route path="/missions" element={<Missions />} />
+        </Routes>
       </div>
     </Router>
   );
