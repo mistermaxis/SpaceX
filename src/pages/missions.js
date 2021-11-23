3add5b9f50f6b412be78ee7c709563c6c6a8984a
Maximiliano <maximiliano_diaz@gmail.com>
2021-11-22 21:07:35 -0300

    Import state from redux store for missions component.

--- src/pages/missions.js
+++ src/pages/missions.js
@@ -1,6 +1,24 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { setMissions } from '../redux/missions';
+
 const Missions = () => {
+  const dispatch = useDispatch();
+  const missions = useSelector(state => state.missionsReducer.missions);
+
+  useEffect(() => {
+    dispatch(setMissions());
+  }, [dispatch]);
+
   return (
-    <h1>Missions</h1>
+    <div>
+      <h1>Missions</h1>
+      {
+        missions.map((mission, index) => {
+          return (<p key={index}>{mission.a}</p>);
+        })
+      }
+    </div>
   );
 }
  
