b78f503437da2c651d510cf4b8756af893ac306c
ixboy <ismaelixboy@gmail.com>
2021-11-24 00:40:32 +0100

    add action creator and reducer for booking rockets

--- src/redux/rockets.js
+++ src/redux/rockets.js
@@ -1,13 +1,30 @@
 import apiGetElements, { GET_ROCKETS } from '../api/api';
 
+const BOOK_ROCKET = 'space-travellers/rockets/BOOK_ROCKET';
+
 const initialState = {
   rockets: [],
 };
+
+export const bookRocket = (id) => ({
+  type: BOOK_ROCKET,
+  id,
+});
 
 const rocketsReducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_ROCKETS:
       return { ...state, rockets: action.rockets };
+
+    case BOOK_ROCKET:
+      return {
+        ...state,
+        rockets: state.rockets.map((rocket) => {
+          if (rocket.id !== action.id) { return rocket; }
+          return { ...rocket, reserved: true };
+        }),
+      };
+
     default:
       return state;
   }
