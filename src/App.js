62ae3b30525fdbb210b5113958ea390eb3e6c0ff
Maximiliano <maximiliano_diaz@gmail.com>
2021-11-23 17:22:18 -0300

    Import profile component.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/navbar';
 import Rockets from './pages/rockets';
 import Missions from './pages/missions';
-import './App.css';
+import Profile from './pages/profile';
 
 function App() {
   return (
@@ -10,8 +10,9 @@ function App() {
       <div className="App">
         <Navbar />
         <Routes>
-          <Route path="/" element={<Rockets />} />
+          <Route exact path="/" element={<Rockets />} />
           <Route path="/missions" element={<Missions />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
       </div>
     </Router>
