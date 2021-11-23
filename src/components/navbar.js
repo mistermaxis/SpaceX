8b41725f95b88859c81553bd3b77d7017bc98b1b
Maximiliano <maximiliano_diaz@gmail.com>
2021-11-22 23:38:34 -0300

    Add escape symbol for apostrophe.

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,20 +2,18 @@ import { NavLink } from 'react-router-dom';
 import Planet from './planet.png';
 import './navbar.css';
 
-const Navbar = () => {
-  return (
-    <nav className="navbar">
-      <div>
-        <img className="logo" src={Planet} alt="" />
-        <h1>Space Traveler's Hub</h1>
-      </div>
-      <menu className="menu">
-        <NavLink to="/">Rockets</NavLink>
-        <NavLink to="missions">Missions</NavLink>
-        <NavLink to="missions">My Profile</NavLink>
-      </menu>
-    </nav>
-  );
-}
- 
+const Navbar = () => (
+  <nav className="navbar">
+    <div>
+      <img className="logo" src={Planet} alt="" />
+      <h1>Space Traveler&aposs Hub</h1>
+    </div>
+    <menu className="menu">
+      <NavLink to="/">Rockets</NavLink>
+      <NavLink to="missions">Missions</NavLink>
+      <NavLink to="missions">My Profile</NavLink>
+    </menu>
+  </nav>
+);
+
 export default Navbar;
