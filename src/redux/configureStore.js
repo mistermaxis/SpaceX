292e63609259a17784c0fdae1ad85e5fb7da378a
Maximiliano <maximiliano_diaz@gmail.com>
2021-11-22 23:37:54 -0300

    Remove js extension from import statements.

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,7 +1,7 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
-import missionsReducer from './missions.js';
+import missionsReducer from './missions';
 
 const reducers = combineReducers({ missionsReducer });
 
