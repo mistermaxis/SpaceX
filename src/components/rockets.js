31161aab8a6275396acf2948b953da3c89f4423f
ixboy <ismaelixboy@gmail.com>
2021-11-24 22:43:57 +0100

    add reserved badge

--- src/components/rockets.js
+++ src/components/rockets.js
@@ -1,6 +1,8 @@
 import { useEffect } from 'react';
+import Badge from 'react-bootstrap/Badge';
 import { useDispatch, useSelector } from 'react-redux';
 import { bookRocket, cancelBooking, fetchRockets } from '../redux/rockets';
+// import Button from 'react-bootstrap/Button';
 
 const RocketList = () => {
   const dispatch = useDispatch();
@@ -13,6 +15,7 @@ const RocketList = () => {
   }, []);
 
   const handleMessage = (reserved) => (reserved ? 'Cancel reservation' : 'reserve Rocket');
+  const handleReserved = (reserved) => (reserved ? 'Reserved' : '');
 
   return (
     <div className="container-fluid">
@@ -23,6 +26,9 @@ const RocketList = () => {
             <div className="">
               <h3>{item.rocket_name}</h3>
               <div className="">
+                <span className="">
+                  <Badge bg="info">{handleReserved(item.reserved)}</Badge>
+                </span>
                 <p>{item.description}</p>
               </div>
               <button type="button" onClick={() => { dispatch(!item.reserved ? bookRocket(item.id) : cancelBooking(item.id)); }}>{handleMessage(item.reserved)}</button>
