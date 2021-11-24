cd8b27a3c0ae82353b034d06701e8f041e5f9e0f
Maximiliano <maximiliano_diaz@gmail.com>
2021-11-24 13:20:31 -0300

    Add code to display joined missions.

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,18 +1,33 @@
+import { useSelector } from 'react-redux';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import ListGroup from 'react-bootstrap/ListGroup';
 
-const MyProfile = () => (
-  <Container className="pt-3">
-    <Row>
-      <Col>
-        <h3>My Missions</h3>
-      </Col>
-      <Col>
-        <h3>My Rockets</h3>
-      </Col>
-    </Row>
-  </Container>
-);
+const MyProfile = () => {
+  const joinedMissions = useSelector((s) => s.missionsReducer.missions.filter((m) => m.joined));
+
+  return (
+    <Container className="pt-3">
+      <Row>
+        <Col>
+          <h3>My Missions</h3>
+          <ListGroup>
+            {
+              joinedMissions.map((mission) => (
+                <ListGroup.Item key={mission.mission_id}>
+                  {mission.mission_name}
+                </ListGroup.Item>
+              ))
+            }
+          </ListGroup>
+        </Col>
+        <Col>
+          <h3>My Rockets</h3>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default MyProfile;
