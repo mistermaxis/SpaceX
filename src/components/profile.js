fba87dc8d6940216cd165913bcdbd3ebbde0059e
Maximiliano <maximiliano_diaz@gmail.com>
2021-11-24 13:11:27 -0300

    Add layout for profile component.

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,5 +1,18 @@
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
 const MyProfile = () => (
-  <h1>My Profile</h1>
+  <Container className="pt-3">
+    <Row>
+      <Col>
+        <h3>My Missions</h3>
+      </Col>
+      <Col>
+        <h3>My Rockets</h3>
+      </Col>
+    </Row>
+  </Container>
 );
 
 export default MyProfile;
