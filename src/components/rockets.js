955ad0b7cdad00431c812ea785f8b65f89be5f1e
ixboy <ismaelixboy@gmail.com>
2021-11-24 02:29:33 +0100

    refactor code and add booking button

--- src/components/rockets.js
+++ src/components/rockets.js
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { fetchRockets } from '../redux/rockets';
+import { useDispatch, useSelector } from 'react-redux';
+import { bookRocket, cancelBooking, fetchRockets } from '../redux/rockets';
 
 const RocketList = () => {
   const dispatch = useDispatch();
@@ -10,20 +10,25 @@ const RocketList = () => {
     dispatch(fetchRockets());
   }, [dispatch]);
 
-  const rocketItem = rockets.map((item) => (
-    <div key={item.id} className="container-fluid d-flex my-4 mx-0 mainDiv">
-      <img className="img-fluid" src={item.flickr_images[0]} alt={item.rocket_name} />
-      <div className="">
-        <h3>{item.rocket_name}</h3>
-        <div className="">
-          <p>{item.description}</p>
-        </div>
-      </div>
-    </div>
-  ));
+  const handleMessage = (reserved) => (reserved ? 'Cancel Booking' : 'Book Rocket');
+
   return (
-    <div className="container">
-      {rocketItem}
+    <div className="container-fluid">
+      {
+        rockets.map((item) => (
+          <div key={item.id} className="container-fluid d-flex my-4 mx-0 mainDiv">
+            <img className="img-fluid" src={item.flickr_images[0]} alt={item.rocket_name} />
+            <div className="">
+              <h3>{item.rocket_name}</h3>
+              <div className="">
+                <p>{item.description}</p>
+              </div>
+              <button type="button" onClick={() => { dispatch(!item.reserved ? bookRocket(item.id) : cancelBooking(item.id)); }}>{handleMessage(item.reserved)}</button>
+            </div>
+          </div>
+        ))
+      }
+      ;
     </div>
   );
 };
