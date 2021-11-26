5dd458b2518ba1d1b153da1720150f4ece446f18
ixboy <ismaelixboy@gmail.com>
2021-11-26 03:56:41 +0100

    add tests to render rocket lists

--- src/ __tests__ /rockets.test.js
+++ src/ __tests__ /rockets.test.js
@@ -1,8 +1,11 @@
+import { render } from '@testing-library/react';
 import React from 'react';
 import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
 import RocketList from '../components/rockets';
 import store from '../redux/configureStore';
+
+jest.mock('../components/rockets')
 
 describe('Group of tests for Rockets page', () => {
 
@@ -10,7 +13,6 @@ describe('Group of tests for Rockets page', () => {
     const list = renderer
       .create(<Provider store={store}><RocketList /></Provider>)
       .toJSON();
-    console.log(list)
     expect(list).toMatchSnapshot();
   });
 
